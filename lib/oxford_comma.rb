0442f5ebee47c226d5cb8b1a1ee013a96725b9e0
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-07 05:34:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,7 @@ def oxford_comma(array)
   elsif array.length() == 2
     array.join(" and ") 
   elsif array.length() >= 3
-    oxford = array[-1].insert(0, "and ")
-    string = array.join(", ")
-    and = ", and " + oxford
-    oxford_string = string + oxford
+    array[-1].insert(0, "and ")
+    array.join(", ")
   end
-  oxford_string
 end
